7e201e203f20480c9b89e8a05d1ccc14e610fdeb
Alexander Zibin <37593964+AlexZibin@users.noreply.github.com>
2018-04-18 17:37:54 +0300

    Update mainstruct.ino

--- mainstruct.ino
+++ mainstruct.ino
@@ -1,6 +1,17 @@
 
 void f1 (void) {
   Serial.println ("f1");
+  
+  /*
+  if (turnLeft ()) {
+  } 
+  elseif (turnRight ()) {
+  }
+  if (shortPress ()) {
+  } 
+  elseif (longPress ()) {
+  }
+  */
 }
 
 void f2 (void) {
@@ -11,12 +22,17 @@ void f3 (void) {
   Serial.println ("f3");
 }
 
-unsigned int currMode (void) {
-  return 0;
-}
-
 void (*modeFunc[])(void) = {&f1, &f2, &f3);
 const int numModes = sizeof(modeFunc)/sizeof(modeFunc)/sizeof(modeFunc[0]);
+
+unsigned int currMode (void) {
+  static int i = 0;
+  
+  if (++i == numModes) {
+    i = 0;
+  }
+  return i;
+}
 
 void setup () {
   initDevices ();
@@ -25,4 +41,11 @@ void setup () {
 
 void loop () {
   modeFunc[currMode ()] ();
+  delay (1000);
 }
+
+void initDevices (void) {
+   Serial.begin (9600);
+}
+
+void readEEPROM (void) {}
