f3c580c40f45f7f4d6adf289b29726794cd5ea57
Alexander Zibin <37593964+AlexZibin@users.noreply.github.com>
2018-04-19 12:22:15 +0300

    Update mainstruct.ino

--- mainstruct.ino
+++ mainstruct.ino
@@ -23,28 +23,40 @@ void f3 (void) {
 }
 
 /*
+// modeChanger.h
 // массив указателей на функции:
-// typedef ........
+// typedef modeFuncArrayPtr ........
 
 class modeChanger {
   public:
-    void modeChanger ();
+    void modeChanger (modeFuncArrayPtr funcArray, unsigned int numModes);
     int currMode (void);
     int nextMode (void);
     int prevMode (void);
     int applyMode (void *newModeFunc(void));
   private:
-}*/
+    int _currMode;
+    unsigned int _numModes;
+    modeFuncArrayPtr _funcArray;
+}
+
+void modeChanger::modeChanger (modeFuncArrayPtr funcArray, unsigned int numModes) {
+  _currMode = 0;
+  _funcArray = _funcArray;
+  _numModes = numModes;
+}
+
+*/
 
 void (*modeFuncArray[])(void) = {&f1, &f2, &f3);
 const int numModes = sizeof(modeFunc)/sizeof(modeFunc)/sizeof(modeFunc[0]);
 
-unsigned int currMode (void) {
+int currMode (void) {
   static int i = 0;
   
-  if (++i == numModes) {
+  /*if (++i == _numModes) {
     i = 0;
-  }
+  }*/
   return i;
 }
 
