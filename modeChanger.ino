199c63a750768e7a9273027eb47f82ad68c5167c
Alexander Zibin <37593964+AlexZibin@users.noreply.github.com>
2018-04-20 12:03:21 +0300

    Update modeChanger.ino

--- modeChanger.ino
+++ modeChanger.ino
@@ -10,16 +10,16 @@ typedef void (*fPtr[])(void);
 
 class modeChanger {
   public:
-    modeChanger (fPtr funcArray);
+    modeChanger (fPtr *funcArray);
   private:
-    *fPtr _funcArray;
+    fPtr *_funcArray;
 };
 
-modeChanger::modeChanger (fPtr funcArray) {
+modeChanger::modeChanger (fPtr *funcArray) {
   _funcArray = funcArray;
 }
 
-static fPtr modeFuncArray[] {f1, f2};
+static fPtr modeFuncArray = {f1, f2};
 const unsigned int numModes = sizeof(modeFuncArray)/sizeof(modeFuncArray[0]);
 
 modeChanger mode = new modeChanger (modeFuncArray);
@@ -33,3 +33,28 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+/* https://ideone.com
+Works like a charm:
+
+#include <iostream>
+
+void f1(void) {
+   std::cout << "f1" << "\n";
+}
+
+void f2(void) {
+   std::cout << "f2" << "\n";
+}
+
+int main() {
+	typedef void (*fPtr[])(void);
+	static fPtr pt = {f1, f2, f2};
+	
+	(*pt[2])();
+	(*pt[1])();
+	(*pt[0])();
+
+	return 0;
+}
+*/
